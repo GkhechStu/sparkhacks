f5e09bcdd06a3c6ba73aaf22decd0781956d5e09
GkhechStu <129134026+GkhechStu@users.noreply.github.com>
2023-04-07 22:45:53 -0500

    Update practiceCV.py
    make a loop for values to print
    commented out extra print statements

--- practiceCV.py
+++ practiceCV.py
@@ -3,7 +3,7 @@
 
 lineHold = ""
 
-def add_element(dict, key, className, time):
+def addVal(dict, key, className, time):
     if key not in dict:
         dict[key] = []
     dict[key].append(className)
@@ -16,36 +16,30 @@ with open("Registration.html", "r") as f:
             lineHold = line
             break
 min = 0
-max = 0
-print(len(lineHold))
 Day = ""
 Time = ""
 counter = 0
 className = ""
 classes = {}
-inc = 0
-# classes["monday"] = {150 : "systems"}
-week = ["Monday ", "Tuesday ", "Wednesday ", "Thursday ", "Friday "]
-
-# add_element(classes, week[0], "hello")
-# add_element(classes, week[0], "hello2")
+week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 end = lineHold.count("class=\"section-time-details\" href=\"#\">")
+
 while counter < end:
     min = lineHold.find("class=\"section-time-details\" href=\"#\">", min) #finds first instance of the this line 
-    print(min) #prints the win
+    # print(min) #prints the win
     min += 38
     Day = lineHold[min : lineHold.find(" ", min)]
-    print(f"Day:*{Day}*")
+    # print(f"Day:*{Day}*")
     Time = lineHold[lineHold.find(" ", min) + 1: lineHold.find("<", min)]
-    print(f"Time:*{Time}*")
+    # print(f"Time:*{Time}*")
     className = lineHold[lineHold.find("</span>", min) + 7 : lineHold.find("</a>", min)]
-    print(f"ClassName:*{className}*")
-    add_element(classes, Day, className, Time)
+    # print(f"ClassName:*{className}*")
+    addVal(classes, Day, className, Time)
     min = lineHold.find("</a>", min)
     counter += 1
 
 print("after run\n")
 
-for i in classes["Monday"]:
-    print(i)
-# </span>Systems Programming</a>
+for i in week:
+    for j in classes[i]:
+        print(j)
