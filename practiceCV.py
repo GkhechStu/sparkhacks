8656774048bd91736911113fe6c6dddeac469af0
Gor Khechyan <gkhechyan24@gmail.com>
2023-04-08 00:52:08 -0500

    completed timeconverter to military time

--- practiceCV.py
+++ practiceCV.py
@@ -3,17 +3,27 @@
 
 def timeConverter(time):
     timeHold = 0
-    if Time[1].isdigit():#convert american time to military 
-        print(Time[0:2])
-    if Time[0] == '1' or  Time[0] == '2' or  Time[0] == '3' or  Time[0] == '4' or  Time[0] == '5' or  Time[0] == '6' or  Time[0] == '6':
-        timeHold = int(Time[0]) + 12
-        print(timeHold)
+    if time[0] == '8' or time[0] == '9' or time[0 : 2] == '10' or time[0 : 2] == '11' or time[0 : 2] == '12':
+        if(time[1].isdigit()):
+            # print("\t10 - 12")
+            time = time[0 : 5] + ":00"
+        else:
+            # print("\tin 8 or 9")
+            time = "0" + time[0 : 4] + ":00"
+        # print(time)
+    elif time[0] == '1' or time[0] == '2' or  time[0] == '3' or  time[0] == '4' or  time[0] == '5' or  time[0] == '6' or  time[0] == '7':
+        # print("\tin 1 to 7")
+        timeHold = int(time[0]) + 12
+        time = str(timeHold) + time[1 : 4] + ":00"
+        # print(time)
+    return time
 
-def addVal(dict, key, className, time):
+def addVal(dict, key, className, timeStart, timeEnd):
     if key not in dict:
         dict[key] = []
     dict[key].append(className)
-    dict[key].append(time)
+    dict[key].append(timeStart)
+    dict[key].append(timeEnd)
 
 classLine = ""
 semLine = ""
@@ -33,6 +43,8 @@ semester = ""
 min = 0
 Day = ""
 Time = ""
+TimeStart = ""
+TimeEnd = ""
 counter = 0
 className = ""
 classes = {}
@@ -51,11 +63,12 @@ while counter < end:
     # print(f"Day:*{Day}*")
     Time = classLine[classLine.find(" ", min) + 1: classLine.find("<", min)]
     # print(f"Time:*{Time}*")
-    timeConverter(Time)
+    TimeStart = timeConverter(Time)
+    TimeEnd = timeConverter(Time[Time.find("- ") + 2 : ])
 
     className = classLine[classLine.find("</span>", min) + 7 : classLine.find("</a>", min)]
     # print(f"ClassName:*{className}*")
-    addVal(classes, Day, className, Time)
+    addVal(classes, Day, className, TimeStart, TimeEnd)
     min = classLine.find("</a>", min)
     counter += 1
 
@@ -66,3 +79,4 @@ print(f"Semmy:*{semester}*\n")
 for i in week:
     for j in classes[i]:
         print(j)
+
