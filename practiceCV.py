bf0eab3084c3259f561f9df37e5d7582ad0e296b
Gor Khechyan <gkhechyan24@gmail.com>
2023-04-07 23:37:35 -0500

    adding timeconverter

--- practiceCV.py
+++ practiceCV.py
@@ -1,7 +1,13 @@
 # Python program to identify
 #color in images
 
-lineHold = ""
+def timeConverter(time):
+    timeHold = 0
+    if Time[1].isdigit():#convert american time to military 
+        print(Time[0:2])
+    if Time[0] == '1' or  Time[0] == '2' or  Time[0] == '3' or  Time[0] == '4' or  Time[0] == '5' or  Time[0] == '6' or  Time[0] == '6':
+        timeHold = int(Time[0]) + 12
+        print(timeHold)
 
 def addVal(dict, key, className, time):
     if key not in dict:
@@ -9,36 +15,53 @@ def addVal(dict, key, className, time):
     dict[key].append(className)
     dict[key].append(time)
 
+classLine = ""
+semLine = ""
+
 with open("Registration.html", "r") as f:
     for line in f.readlines():
         if "id=\"calendar\"" in line:
             # print(line,"\n")
-            lineHold = line
-            break
+            classLine = line
+        if "class=\"schedule-list-view-title-text\">" in line:
+        # print(line,"\n")
+            semLine = line
+
+# print(semLine)
+# ---------------------------------------- var declarations below ----------------------------------------
+semester = ""
 min = 0
 Day = ""
 Time = ""
 counter = 0
 className = ""
 classes = {}
+# ---------------------------------------- var declarations above ----------------------------------------
+
+semester = semLine[semLine.find(">") + 1 : semLine.find("</span>")]
+
 week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-end = lineHold.count("class=\"section-time-details\" href=\"#\">")
+end = classLine.count("class=\"section-time-details\" href=\"#\">")
 
 while counter < end:
-    min = lineHold.find("class=\"section-time-details\" href=\"#\">", min) #finds first instance of the this line 
+    min = classLine.find("class=\"section-time-details\" href=\"#\">", min) #finds first instance of the this line 
     # print(min) #prints the win
     min += 38
-    Day = lineHold[min : lineHold.find(" ", min)]
+    Day = classLine[min : classLine.find(" ", min)]
     # print(f"Day:*{Day}*")
-    Time = lineHold[lineHold.find(" ", min) + 1: lineHold.find("<", min)]
+    Time = classLine[classLine.find(" ", min) + 1: classLine.find("<", min)]
     # print(f"Time:*{Time}*")
-    className = lineHold[lineHold.find("</span>", min) + 7 : lineHold.find("</a>", min)]
+    timeConverter(Time)
+
+    className = classLine[classLine.find("</span>", min) + 7 : classLine.find("</a>", min)]
     # print(f"ClassName:*{className}*")
     addVal(classes, Day, className, Time)
-    min = lineHold.find("</a>", min)
+    min = classLine.find("</a>", min)
     counter += 1
 
 print("after run\n")
+
+print(f"Semmy:*{semester}*\n")
 
 for i in week:
     for j in classes[i]:
